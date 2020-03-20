0de608243b40847039c8f704e29971389e9f10ec
sanved77 <sanved77@gmail.com>
2020-03-19 22:38:39 -0400

    Fixed the articles for marquee text

--- js/init.js
+++ js/init.js
@@ -512,7 +512,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Software Developer", "Android Developer", "Node Developer", "React Developer", "Vue Developer"],
+			strings: ["a Software Developer", "an Android Developer", "a Node Developer", "a React Developer", "a Vue Developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
